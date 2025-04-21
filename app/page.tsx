6e85e9236abb4e9d5190b26c0b3686fb78431629
abdulakhatov-tech <islomabdulakhatov6975@gmail.com>
2025-04-21 14:51:30 +0500

    chore: setup Husky, Biome, and Github Actions for linting and CI/CD

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Welcome to Asadbek Abduvoitov Website</h1>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<h1 className="text-4xl font-bold">
+				Welcome to Asadbek Abduvoitov Website
+			</h1>
+		</div>
+	);
 }
