5e5d03bf45424ba3b192ee5861b797f38255d7f1
Asadbek Abduvoitov <161235406+asadbekabduvoitov-nordra@users.noreply.github.com>
2025-04-26 22:54:58 +0500

    feat: added apis for blog CRUD

--- app/api/blog/[slug]/route.ts
+++ app/api/blog/[slug]/route.ts
@@ -3,19 +3,25 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function GET(
-	_: NextRequest,
-	{ params }: { params: Promise<{ slug: string }> },
+	req: NextRequest,
+	{ params }: { params: { slug: string } },
 ) {
-	const { slug } = await params;
+	const { slug } = params;
+
+	// Get search params from URL
+	const { searchParams } = new URL(req.url);
+	const lang = searchParams.get("lang")?.toUpperCase() || "UZ";
 
 	const { data, error } = await supabase
 		.from("blogs")
 		.select("*")
 		.eq("slug", slug)
+		.eq("lang", lang) // filter by lang too
 		.single();
 
-	if (error)
+	if (error) {
 		return NextResponse.json({ error: "Blog not found" }, { status: 404 });
+	}
 
 	return NextResponse.json(data);
 }
