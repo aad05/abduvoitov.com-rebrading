8978e0146fe2f1e36f6953b9ab445ea367fac2f9
Asadbek Abduvoitov <161235406+asadbekabduvoitov-nordra@users.noreply.github.com>
2025-04-26 23:17:37 +0500

    feat: added apis for blog CRUD

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,40 +1,42 @@
 // import { storage } from "@/configs/firebase-admin";
-// import { type NextRequest, NextResponse } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 // import { v4 as uuidv4 } from "uuid";
 
-// export async function POST(req: NextRequest) {
-// 	// Check if the content type is multipart/form-data
-// 	const contentType = req.headers.get("content-type") || "";
+export async function POST(req: NextRequest) {
+	return NextResponse.json({ url: "hello" });
 
-// 	if (!contentType.includes("multipart/form-data")) {
-// 		return NextResponse.json(
-// 			{ error: "Invalid content-type, expected multipart/form-data" },
-// 			{ status: 400 },
-// 		);
-// 	}
+	// Check if the content type is multipart/form-data
+	// const contentType = req.headers.get("content-type") || "";
 
-// 	const formData = await req.formData();
-// 	const file = formData.get("file") as File | null;
+	// if (!contentType.includes("multipart/form-data")) {
+	// 	return NextResponse.json(
+	// 		{ error: "Invalid content-type, expected multipart/form-data" },
+	// 		{ status: 400 },
+	// 	);
+	// }
 
-// 	if (!file) {
-// 		return NextResponse.json({ error: "No file uploaded." }, { status: 400 });
-// 	}
+	// const formData = await req.formData();
+	// const file = formData.get("file") as File | null;
 
-// 	const bytes = await file.arrayBuffer();
-// 	const buffer = Buffer.from(bytes);
+	// if (!file) {
+	// 	return NextResponse.json({ error: "No file uploaded." }, { status: 400 });
+	// }
 
-// 	const filename = `blogs/${uuidv4()}-${file.name}`;
-// 	const fileRef = storage.file(filename);
+	// const bytes = await file.arrayBuffer();
+	// const buffer = Buffer.from(bytes);
 
-// 	await fileRef.save(buffer, {
-// 		metadata: {
-// 			contentType: file.type,
-// 		},
-// 		public: true,
-// 		resumable: false,
-// 	});
+	// const filename = `blogs/${uuidv4()}-${file.name}`;
+	// const fileRef = storage.file(filename);
 
-// 	const publicUrl = `https://storage.googleapis.com/${process.env.FIREBASE_STORAGE_BUCKET}/${filename}`;
+	// await fileRef.save(buffer, {
+	// 	metadata: {
+	// 		contentType: file.type,
+	// 	},
+	// 	public: true,
+	// 	resumable: false,
+	// });
 
-// 	return NextResponse.json({ url: publicUrl });
-// }
+	// const publicUrl = `https://storage.googleapis.com/${process.env.FIREBASE_STORAGE_BUCKET}/${filename}`;
+
+	// return NextResponse.json({ url: publicUrl });
+}
